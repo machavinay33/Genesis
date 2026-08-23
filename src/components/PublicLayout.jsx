d7d88f37830c7053667919e0818182e114a89e95
machavinay33 <7.4344507e+07+machavinay33@users.noreply.github.com>
2026-08-23 00:25:27 +0000

    Remove route transition navigation glitch

--- src/components/PublicLayout.jsx
+++ src/components/PublicLayout.jsx
@@ -1,14 +1,12 @@
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar.jsx'
 import Footer from './Footer.jsx'
 
 export default function PublicLayout() {
-  const { pathname } = useLocation()
-
   return (
     <div className="flex min-h-screen flex-col bg-void">
       <Navbar />
-      <main key={pathname} className="page-transition flex-1">
+      <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
